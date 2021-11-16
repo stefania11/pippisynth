c112584cc6e1ac03332cbbabb3beccd310ce29b5
Darya Verzhbinsky <darya99@gmail.com>
2021-11-15 17:26:29 -0800

    added tests size 2-20 in rkt files

--- synthesis-protype/output-files/emina-trial.rkt
+++ synthesis-protype/output-files/emina-trial.rkt
@@ -42,6 +42,273 @@
 
 (define outer-string "")
 (set! outer-string (string-append outer-string "type,size,time,program\n"))
+
+
+; -------------------------------------------------------------
+(define prog4 (list
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+)) 
+
+(define before4 (current-inexact-milliseconds))
+(define sol4
+  (synthesize
+     #:forall in
+     #:guarantee
+     (let ([out (interpret prog4 in)])
+       (assert (= (point-x out) (- (point-x in) 2)))
+       (assert (= (point-y out) (- (point-y in) 2))))))
+
+(define after4 (current-inexact-milliseconds))
+(define time4 (- after4 before4))
+(set! outer-string (string-append outer-string "emina,4,"))
+(set! outer-string (string-append outer-string (~s time4)))
+(set! outer-string (string-append outer-string ","))
+(set! outer-string (string-append outer-string (~s (evaluate prog4 sol4))))
+(set! outer-string (string-append outer-string "\n"))
+
+
+; -------------------------------------------------------------
+(define prog6 (list
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+)) 
+
+(define before6 (current-inexact-milliseconds))
+(define sol6
+  (synthesize
+     #:forall in
+     #:guarantee
+     (let ([out (interpret prog6 in)])
+       (assert (= (point-x out) (- (point-x in) 3)))
+       (assert (= (point-y out) (- (point-y in) 3))))))
+
+(define after6 (current-inexact-milliseconds))
+(define time6 (- after6 before6))
+(set! outer-string (string-append outer-string "emina,6,"))
+(set! outer-string (string-append outer-string (~s time6)))
+(set! outer-string (string-append outer-string ","))
+(set! outer-string (string-append outer-string (~s (evaluate prog6 sol6))))
+(set! outer-string (string-append outer-string "\n"))
+
+
+; -------------------------------------------------------------
+(define prog8 (list
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+)) 
+
+(define before8 (current-inexact-milliseconds))
+(define sol8
+  (synthesize
+     #:forall in
+     #:guarantee
+     (let ([out (interpret prog8 in)])
+       (assert (= (point-x out) (- (point-x in) 4)))
+       (assert (= (point-y out) (- (point-y in) 4))))))
+
+(define after8 (current-inexact-milliseconds))
+(define time8 (- after8 before8))
+(set! outer-string (string-append outer-string "emina,8,"))
+(set! outer-string (string-append outer-string (~s time8)))
+(set! outer-string (string-append outer-string ","))
+(set! outer-string (string-append outer-string (~s (evaluate prog8 sol8))))
+(set! outer-string (string-append outer-string "\n"))
+
+
+; -------------------------------------------------------------
+(define prog10 (list
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+)) 
+
+(define before10 (current-inexact-milliseconds))
+(define sol10
+  (synthesize
+     #:forall in
+     #:guarantee
+     (let ([out (interpret prog10 in)])
+       (assert (= (point-x out) (- (point-x in) 5)))
+       (assert (= (point-y out) (- (point-y in) 5))))))
+
+(define after10 (current-inexact-milliseconds))
+(define time10 (- after10 before10))
+(set! outer-string (string-append outer-string "emina,10,"))
+(set! outer-string (string-append outer-string (~s time10)))
+(set! outer-string (string-append outer-string ","))
+(set! outer-string (string-append outer-string (~s (evaluate prog10 sol10))))
+(set! outer-string (string-append outer-string "\n"))
+
+
+; -------------------------------------------------------------
+(define prog12 (list
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+)) 
+
+(define before12 (current-inexact-milliseconds))
+(define sol12
+  (synthesize
+     #:forall in
+     #:guarantee
+     (let ([out (interpret prog12 in)])
+       (assert (= (point-x out) (- (point-x in) 6)))
+       (assert (= (point-y out) (- (point-y in) 6))))))
+
+(define after12 (current-inexact-milliseconds))
+(define time12 (- after12 before12))
+(set! outer-string (string-append outer-string "emina,12,"))
+(set! outer-string (string-append outer-string (~s time12)))
+(set! outer-string (string-append outer-string ","))
+(set! outer-string (string-append outer-string (~s (evaluate prog12 sol12))))
+(set! outer-string (string-append outer-string "\n"))
+
+
+; -------------------------------------------------------------
+(define prog14 (list
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+)) 
+
+(define before14 (current-inexact-milliseconds))
+(define sol14
+  (synthesize
+     #:forall in
+     #:guarantee
+     (let ([out (interpret prog14 in)])
+       (assert (= (point-x out) (- (point-x in) 7)))
+       (assert (= (point-y out) (- (point-y in) 7))))))
+
+(define after14 (current-inexact-milliseconds))
+(define time14 (- after14 before14))
+(set! outer-string (string-append outer-string "emina,14,"))
+(set! outer-string (string-append outer-string (~s time14)))
+(set! outer-string (string-append outer-string ","))
+(set! outer-string (string-append outer-string (~s (evaluate prog14 sol14))))
+(set! outer-string (string-append outer-string "\n"))
+
+
+; -------------------------------------------------------------
+(define prog16 (list
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+)) 
+
+(define before16 (current-inexact-milliseconds))
+(define sol16
+  (synthesize
+     #:forall in
+     #:guarantee
+     (let ([out (interpret prog16 in)])
+       (assert (= (point-x out) (- (point-x in) 8)))
+       (assert (= (point-y out) (- (point-y in) 8))))))
+
+(define after16 (current-inexact-milliseconds))
+(define time16 (- after16 before16))
+(set! outer-string (string-append outer-string "emina,16,"))
+(set! outer-string (string-append outer-string (~s time16)))
+(set! outer-string (string-append outer-string ","))
+(set! outer-string (string-append outer-string (~s (evaluate prog16 sol16))))
+(set! outer-string (string-append outer-string "\n"))
+
+
+; -------------------------------------------------------------
+(define prog18 (list
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+  (sym-op)
+)) 
+
+(define before18 (current-inexact-milliseconds))
+(define sol18
+  (synthesize
+     #:forall in
+     #:guarantee
+     (let ([out (interpret prog18 in)])
+       (assert (= (point-x out) (- (point-x in) 9)))
+       (assert (= (point-y out) (- (point-y in) 9))))))
+
+(define after18 (current-inexact-milliseconds))
+(define time18 (- after18 before18))
+(set! outer-string (string-append outer-string "emina,18,"))
+(set! outer-string (string-append outer-string (~s time18)))
+(set! outer-string (string-append outer-string ","))
+(set! outer-string (string-append outer-string (~s (evaluate prog18 sol18))))
+(set! outer-string (string-append outer-string "\n"))
+
+
+; -------------------------------------------------------------
 (define prog20 (list
   (sym-op)
   (sym-op)
--- synthesis-protype/output-files/ras-trial.rkt
+++ synthesis-protype/output-files/ras-trial.rkt
@@ -22,6 +22,248 @@
 
 (define outer-string "")
 (set! outer-string (string-append outer-string "type,size,time,program\n"))
+
+; -------------------------------------------------------------
+(define (prog-sketch4 coord0)
+  (define coord1 (moving coord0   #:depth 1)) 
+  (define coord2 (moving coord1   #:depth 1)) 
+  (define coord3 (moving coord2   #:depth 1)) 
+  (define coord4 (moving coord3   #:depth 1)) 
+  coord4
+)
+(define before4 (current-inexact-milliseconds))
+
+(define sol4
+  (synthesize
+      #:forall symbol-coord
+      #:guarantee ((curry checker 2) prog-sketch4 symbol-coord)))
+
+
+(define after4 (current-inexact-milliseconds))
+(define time4 (- after4 before4))
+(set! outer-string (string-append outer-string "ras,4,"))
+(set! outer-string (string-append outer-string (~s time4)))
+(set! outer-string (string-append outer-string "\n"))
+(print-forms sol4)
+
+; -------------------------------------------------------------
+(define (prog-sketch6 coord0)
+  (define coord1 (moving coord0   #:depth 1)) 
+  (define coord2 (moving coord1   #:depth 1)) 
+  (define coord3 (moving coord2   #:depth 1)) 
+  (define coord4 (moving coord3   #:depth 1)) 
+  (define coord5 (moving coord4   #:depth 1)) 
+  (define coord6 (moving coord5   #:depth 1)) 
+  coord6
+)
+(define before6 (current-inexact-milliseconds))
+
+(define sol6
+  (synthesize
+      #:forall symbol-coord
+      #:guarantee ((curry checker 3) prog-sketch6 symbol-coord)))
+
+
+(define after6 (current-inexact-milliseconds))
+(define time6 (- after6 before6))
+(set! outer-string (string-append outer-string "ras,6,"))
+(set! outer-string (string-append outer-string (~s time6)))
+(set! outer-string (string-append outer-string "\n"))
+(print-forms sol6)
+
+; -------------------------------------------------------------
+(define (prog-sketch8 coord0)
+  (define coord1 (moving coord0   #:depth 1)) 
+  (define coord2 (moving coord1   #:depth 1)) 
+  (define coord3 (moving coord2   #:depth 1)) 
+  (define coord4 (moving coord3   #:depth 1)) 
+  (define coord5 (moving coord4   #:depth 1)) 
+  (define coord6 (moving coord5   #:depth 1)) 
+  (define coord7 (moving coord6   #:depth 1)) 
+  (define coord8 (moving coord7   #:depth 1)) 
+  coord8
+)
+(define before8 (current-inexact-milliseconds))
+
+(define sol8
+  (synthesize
+      #:forall symbol-coord
+      #:guarantee ((curry checker 4) prog-sketch8 symbol-coord)))
+
+
+(define after8 (current-inexact-milliseconds))
+(define time8 (- after8 before8))
+(set! outer-string (string-append outer-string "ras,8,"))
+(set! outer-string (string-append outer-string (~s time8)))
+(set! outer-string (string-append outer-string "\n"))
+(print-forms sol8)
+
+; -------------------------------------------------------------
+(define (prog-sketch10 coord0)
+  (define coord1 (moving coord0   #:depth 1)) 
+  (define coord2 (moving coord1   #:depth 1)) 
+  (define coord3 (moving coord2   #:depth 1)) 
+  (define coord4 (moving coord3   #:depth 1)) 
+  (define coord5 (moving coord4   #:depth 1)) 
+  (define coord6 (moving coord5   #:depth 1)) 
+  (define coord7 (moving coord6   #:depth 1)) 
+  (define coord8 (moving coord7   #:depth 1)) 
+  (define coord9 (moving coord8   #:depth 1)) 
+  (define coord10 (moving coord9   #:depth 1)) 
+  coord10
+)
+(define before10 (current-inexact-milliseconds))
+
+(define sol10
+  (synthesize
+      #:forall symbol-coord
+      #:guarantee ((curry checker 5) prog-sketch10 symbol-coord)))
+
+
+(define after10 (current-inexact-milliseconds))
+(define time10 (- after10 before10))
+(set! outer-string (string-append outer-string "ras,10,"))
+(set! outer-string (string-append outer-string (~s time10)))
+(set! outer-string (string-append outer-string "\n"))
+(print-forms sol10)
+
+; -------------------------------------------------------------
+(define (prog-sketch12 coord0)
+  (define coord1 (moving coord0   #:depth 1)) 
+  (define coord2 (moving coord1   #:depth 1)) 
+  (define coord3 (moving coord2   #:depth 1)) 
+  (define coord4 (moving coord3   #:depth 1)) 
+  (define coord5 (moving coord4   #:depth 1)) 
+  (define coord6 (moving coord5   #:depth 1)) 
+  (define coord7 (moving coord6   #:depth 1)) 
+  (define coord8 (moving coord7   #:depth 1)) 
+  (define coord9 (moving coord8   #:depth 1)) 
+  (define coord10 (moving coord9   #:depth 1)) 
+  (define coord11 (moving coord10   #:depth 1)) 
+  (define coord12 (moving coord11   #:depth 1)) 
+  coord12
+)
+(define before12 (current-inexact-milliseconds))
+
+(define sol12
+  (synthesize
+      #:forall symbol-coord
+      #:guarantee ((curry checker 6) prog-sketch12 symbol-coord)))
+
+
+(define after12 (current-inexact-milliseconds))
+(define time12 (- after12 before12))
+(set! outer-string (string-append outer-string "ras,12,"))
+(set! outer-string (string-append outer-string (~s time12)))
+(set! outer-string (string-append outer-string "\n"))
+(print-forms sol12)
+
+; -------------------------------------------------------------
+(define (prog-sketch14 coord0)
+  (define coord1 (moving coord0   #:depth 1)) 
+  (define coord2 (moving coord1   #:depth 1)) 
+  (define coord3 (moving coord2   #:depth 1)) 
+  (define coord4 (moving coord3   #:depth 1)) 
+  (define coord5 (moving coord4   #:depth 1)) 
+  (define coord6 (moving coord5   #:depth 1)) 
+  (define coord7 (moving coord6   #:depth 1)) 
+  (define coord8 (moving coord7   #:depth 1)) 
+  (define coord9 (moving coord8   #:depth 1)) 
+  (define coord10 (moving coord9   #:depth 1)) 
+  (define coord11 (moving coord10   #:depth 1)) 
+  (define coord12 (moving coord11   #:depth 1)) 
+  (define coord13 (moving coord12   #:depth 1)) 
+  (define coord14 (moving coord13   #:depth 1)) 
+  coord14
+)
+(define before14 (current-inexact-milliseconds))
+
+(define sol14
+  (synthesize
+      #:forall symbol-coord
+      #:guarantee ((curry checker 7) prog-sketch14 symbol-coord)))
+
+
+(define after14 (current-inexact-milliseconds))
+(define time14 (- after14 before14))
+(set! outer-string (string-append outer-string "ras,14,"))
+(set! outer-string (string-append outer-string (~s time14)))
+(set! outer-string (string-append outer-string "\n"))
+(print-forms sol14)
+
+; -------------------------------------------------------------
+(define (prog-sketch16 coord0)
+  (define coord1 (moving coord0   #:depth 1)) 
+  (define coord2 (moving coord1   #:depth 1)) 
+  (define coord3 (moving coord2   #:depth 1)) 
+  (define coord4 (moving coord3   #:depth 1)) 
+  (define coord5 (moving coord4   #:depth 1)) 
+  (define coord6 (moving coord5   #:depth 1)) 
+  (define coord7 (moving coord6   #:depth 1)) 
+  (define coord8 (moving coord7   #:depth 1)) 
+  (define coord9 (moving coord8   #:depth 1)) 
+  (define coord10 (moving coord9   #:depth 1)) 
+  (define coord11 (moving coord10   #:depth 1)) 
+  (define coord12 (moving coord11   #:depth 1)) 
+  (define coord13 (moving coord12   #:depth 1)) 
+  (define coord14 (moving coord13   #:depth 1)) 
+  (define coord15 (moving coord14   #:depth 1)) 
+  (define coord16 (moving coord15   #:depth 1)) 
+  coord16
+)
+(define before16 (current-inexact-milliseconds))
+
+(define sol16
+  (synthesize
+      #:forall symbol-coord
+      #:guarantee ((curry checker 8) prog-sketch16 symbol-coord)))
+
+
+(define after16 (current-inexact-milliseconds))
+(define time16 (- after16 before16))
+(set! outer-string (string-append outer-string "ras,16,"))
+(set! outer-string (string-append outer-string (~s time16)))
+(set! outer-string (string-append outer-string "\n"))
+(print-forms sol16)
+
+; -------------------------------------------------------------
+(define (prog-sketch18 coord0)
+  (define coord1 (moving coord0   #:depth 1)) 
+  (define coord2 (moving coord1   #:depth 1)) 
+  (define coord3 (moving coord2   #:depth 1)) 
+  (define coord4 (moving coord3   #:depth 1)) 
+  (define coord5 (moving coord4   #:depth 1)) 
+  (define coord6 (moving coord5   #:depth 1)) 
+  (define coord7 (moving coord6   #:depth 1)) 
+  (define coord8 (moving coord7   #:depth 1)) 
+  (define coord9 (moving coord8   #:depth 1)) 
+  (define coord10 (moving coord9   #:depth 1)) 
+  (define coord11 (moving coord10   #:depth 1)) 
+  (define coord12 (moving coord11   #:depth 1)) 
+  (define coord13 (moving coord12   #:depth 1)) 
+  (define coord14 (moving coord13   #:depth 1)) 
+  (define coord15 (moving coord14   #:depth 1)) 
+  (define coord16 (moving coord15   #:depth 1)) 
+  (define coord17 (moving coord16   #:depth 1)) 
+  (define coord18 (moving coord17   #:depth 1)) 
+  coord18
+)
+(define before18 (current-inexact-milliseconds))
+
+(define sol18
+  (synthesize
+      #:forall symbol-coord
+      #:guarantee ((curry checker 9) prog-sketch18 symbol-coord)))
+
+
+(define after18 (current-inexact-milliseconds))
+(define time18 (- after18 before18))
+(set! outer-string (string-append outer-string "ras,18,"))
+(set! outer-string (string-append outer-string (~s time18)))
+(set! outer-string (string-append outer-string "\n"))
+(print-forms sol18)
+
+; -------------------------------------------------------------
 (define (prog-sketch20 coord0)
   (define coord1 (moving coord0   #:depth 1)) 
   (define coord2 (moving coord1   #:depth 1)) 
@@ -58,4 +300,5 @@
 (set! outer-string (string-append outer-string "ras,20,"))
 (set! outer-string (string-append outer-string (~s time20)))
 (set! outer-string (string-append outer-string "\n"))
+(print-forms sol20)
 (write-file "output-files/ras.csv" outer-string)
