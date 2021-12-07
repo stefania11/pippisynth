4cf1599f90280e3c453074a232b15ef8c6d9616d
Darya Verzhbinsky <darya99@gmail.com>
2021-12-06 18:58:23 -0800

    added 1 more example

--- synthesis-protype/looping-synthesis.rkt
+++ synthesis-protype/looping-synthesis.rkt
@@ -1,4 +1,5 @@
-#lang rosette/safe
+#lang rosette
+; #lang rosette/safe
 
 (require
   rosette/lib/synthax
@@ -8,6 +9,10 @@
 (require 
   "moving-grammar.rkt" 
 )
+
+; (require 2htdp/batch-io)
+(require racket/sandbox)
+
 
 ; Define a counting loop:
 ; defines a loop that iterates up to n times, calling body in each iteration, and
@@ -26,7 +31,7 @@
 ;
 
 (define (fresh-sym-coord) 
-  (define-symbolic* x y integer?)   ; note the '*' in define-symbolic*
+  (define-symbolic* x y integer?)
   (list x y))
 
 (define (coord-equals coord coord2)
@@ -34,12 +39,6 @@
     [(list x y x2 y2)
      (and (= x x2)
           (= y y2))]))
-
-(define (coord-x coord)
-  (list-ref coord 0))
-
-(define (coord-y coord)
-  (list-ref coord 1))
 
 (define (same-x coord1 coord2)
   (destruct (append coord1 coord2)
@@ -51,121 +50,232 @@
     [(list x1 y1 x2 y2)
       (= y1 y2)]))
 
+; (coord2) ... (coord1)
+(define (is-to-right coord1 coord2)
+  (destruct (append coord1 coord2)
+    [(list x1 y1 x2 y2)
+        (> x1 x2)
+    ]
+  )
+)
+
+; (coord1) ... (coord2)
+(define (is-to-left coord1 coord2)
+  (destruct (append coord1 coord2)
+    [(list x1 y1 x2 y2)
+      (< x1 x2)
+    ]
+  )
+)
+
+; (coord2) 
+; ...
+; (coord1)
+(define (is-below coord1 coord2)
+  (destruct (append coord1 coord2)
+    [(list x1 y1 x2 y2)
+      (> y1 y2)
+    ]
+  )
+)
+
+; (coord1) 
+; ...
+; (coord2)
+(define (is-above coord1 coord2)
+  (destruct (append coord1 coord2)
+    [(list x1 y1 x2 y2)
+      (< y1 y2)
+    ]
+  )
+)
+
+
 ;
-; Conditional grammar (new!!)
+; Conditional grammar
 ;
 
 (define-grammar (bi-conditional coord1 coord2)
     [expr
-        (choose (same-x coord1 coord2)
+        (choose 
+                (same-x coord1 coord2)
                 (same-y coord1 coord2)
+
+                (is-to-right coord1 coord2)
+                (is-to-left coord1 coord2)
+                (is-below coord1 coord2)
+                (is-above coord1 coord2)
         )
     ]
 )
 
-;
-; Various board position predicates 
-;
-
-(define top-row 0)
-(define bottom-row 250)
-(define left-col 0)
-(define right-col 250)
-
-; (new!!) different than `is-out-of-bounds`
-;   difference here is that we're checking if it is AT the bounds
-;   instead of already outside the bounds
-;   I (darya) find this to be a better checker 
-;   (we want to stop AT bounds, not once we're already outside them)
-;   THIS ISN'T USED BUT I WANTED TO INCLUDE FOR FURTHER EXPLORATION
-(define (is-at-bounds coord)
-  (destruct coord
-    [(list x y) (or 
-        (= y top-row)
-        (= y bottom-row)
-        (= x left-col)
-        (= x right-col)
-    )]))
     
 ;
 ; SKETCHES
 ;
 
-(define (sk-meet coord-start coord-end)
+; sketch for moving from `coord-start` to `coord-end`
+;    ONLY in 1 quadrant
+(define (sk-meet-1-quad range coord-start coord-end)
   (define coord-curr coord-start)
-  (for-loop 7
+  (for-loop (* 2 range)
     (lambda () (bi-conditional coord-curr coord-end #:depth 1))
     (lambda () (set! coord-curr (moving coord-curr #:depth 1))))
-  (for-loop 7
+  (for-loop (* 2 range)
     (lambda () (bi-conditional coord-curr coord-end #:depth 1))
     (lambda () (set! coord-curr (moving coord-curr #:depth 1))))
   coord-curr
 )
 
+#|
+solution:
+
+(define (sk-meet-1-quad-sol range coord-start coord-end)
+   (define coord-curr coord-start)
+   (for-loop (* 2 range)
+    (lambda () (same-x coord-curr coord-end))
+    (lambda () (set! coord-curr (moveRight coord-curr))))
+   (for-loop (* 2 range)
+    (lambda () (same-y coord-curr coord-end))
+    (lambda () (set! coord-curr (moveDown coord-curr))))
+   coord-curr)
+
+|#
+
+; sketch for moving from `coord-start` to `coord-end`
+;    works for all 4 quadrants
+(define (sk-meet-4-quads range coord-start coord-end)
+  (define coord-curr coord-start)
+  (for-loop (* 2 range)
+    (lambda () (coord-equals coord-curr coord-end))
+    (lambda () (cond
+        [(bi-conditional coord-curr coord-end #:depth 1)
+            (set! coord-curr (moving coord-curr #:depth 1))]
+        [(bi-conditional coord-curr coord-end #:depth 1)
+            (set! coord-curr (moving coord-curr #:depth 1))]
+        [(bi-conditional coord-curr coord-end #:depth 1)
+            (set! coord-curr (moving coord-curr #:depth 1))]
+        [(bi-conditional coord-curr coord-end #:depth 1)
+            (set! coord-curr (moving coord-curr #:depth 1))])))
+  coord-curr
+)
+
+#|
+solution:
+
+(define (sk-meet-4-quads-sol range coord-start coord-end)
+   (define coord-curr coord-start)
+   (for-loop (* 2 range)
+    (lambda () (coord-equals coord-curr coord-end))
+    (lambda ()
+      (cond
+       [(is-above coord-curr coord-end)
+          (set! coord-curr (moveDown coord-curr))]
+       [(is-below coord-curr coord-end) 
+          (set! coord-curr (moveUp coord-curr))]
+       [(is-to-left coord-curr coord-end)
+          (set! coord-curr (moveRight coord-curr))]
+       [(is-to-right coord-curr coord-end)
+          (set! coord-curr (moveLeft coord-curr))])))
+   coord-curr)
+
+|#
+
+
+
 ;
 ; CHECKERS
 ;
 
-(define (checker-same-bi impl coord-start coord-end)
+(define (checker-1-quad range impl coord-start coord-end)
   (destruct (append coord-start coord-end)
     [(list x1 y1 x2 y2)
 
-     (define new-coord (impl coord-start coord-end))
+      (define new-coord (impl range coord-start coord-end))
 
-     (assert (implies 
+      (assert (implies 
+
+                ; the following ensures we have the correct
+                ;    starting constraints to be able to synthesize
+                ;    our goal
                 (and
-                    ; this is saying that (x2-7) <= x1 <= x2
+                    ; this is saying that (x2-range) <= x1 <= x2
                     (<= x1 x2)
-                    (>= x1 (- x2 7))
+                    (>= x1 (- x2 range))
 
-                    ; this is saying that (y2-7) <= y1 <= y2
+                    ; this is saying that (y2-range) <= y1 <= y2
                     (<= y1 y2)
-                    (>= y1 (- y2 7))
+                    (>= y1 (- y2 range))
                 )
+
                 (coord-equals new-coord coord-end)
-             ))
+              )
+      )
     ]
   )
 )
+
+(define (checker-4-quads range impl coord-start coord-end)
+  (destruct (append coord-start coord-end)
+    [(list x1 y1 x2 y2)
+
+      (define new-coord (impl range coord-start coord-end))
+
+      (assert (implies 
+
+                ; the following ensures we have the correct
+                ;    starting constraints to be able to synthesize
+                ;    our goal
+                (and
+                    ; this is saying that (x2-range) <= x1 <= (x2+range)
+                    (<= x1 (+ x2 range))
+                    (>= x1 (- x2 range))
+
+                    ; this is saying that (y2-range) <= y1 <= (y2+range)
+                    (<= y1 (+ y2 range))
+                    (>= y1 (- y2 range))
+                )
+
+                (coord-equals new-coord coord-end)
+              )
+      )
+    ]
+  )
+)
+
 
 ;
 ; SOLVERS
 ;
 
-(define (do-synthesis-bi sketch)
+(define (do-synthesis-bi range checker sketch)
 
     (define symbol-coord-start (fresh-sym-coord))
     (define symbol-coord-end   (fresh-sym-coord))
 
+    (define before (current-inexact-milliseconds))
     (define sol-same
       (synthesize
               #:forall (list symbol-coord-start symbol-coord-end)
-              #:guarantee (checker-same-bi sketch symbol-coord-start symbol-coord-end)))
-                        
-    (printf "\n\n=================\n")
+              #:guarantee (checker range sketch symbol-coord-start symbol-coord-end)))
+    
+    (define after (current-inexact-milliseconds))
+    (define time (- after before))
+
     (if (sat? sol-same) 
             (begin 
                 (printf "solution:\n")
                 (print-forms sol-same))
             (begin
                 (printf "no solution found\n")))
-)
+) 
 
-(do-synthesis-bi sk-meet)
 
-#|
-SOLUTION:
+;
+; RUNNING SYNTHESIS
+;
 
-(define (sk-meet coord-start coord-end)
-   (define coord-curr coord-start)
-   (for-loop
-    7
-    (lambda () (same-x coord-curr coord-end))
-    (lambda () (set! coord-curr (moveRight coord-curr))))
-   (for-loop
-    7
-    (lambda () (same-y coord-curr coord-end))
-    (lambda () (set! coord-curr (moveDown coord-curr))))
-   coord-curr)
-
-|#
+(define range 7)
+(do-synthesis-bi range checker-1-quad sk-meet-1-quad)
+(newline)
+(do-synthesis-bi range checker-4-quads sk-meet-4-quads)
